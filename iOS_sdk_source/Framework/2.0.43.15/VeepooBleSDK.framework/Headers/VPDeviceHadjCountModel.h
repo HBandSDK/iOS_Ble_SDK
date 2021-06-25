49110e074e02afb8cca5e406068ec4552dccce62
veepoo <40560874+helloveepoo@users.noreply.github.com>
2021-06-25 17:48:57 +0800

    SDK更新诵经

--- iOS_sdk_source/Framework/2.0.43.15/VeepooBleSDK.framework/Headers/VPDeviceHadjCountModel.h
+++ iOS_sdk_source/Framework/2.0.43.15/VeepooBleSDK.framework/Headers/VPDeviceHadjCountModel.h
@@ -12,7 +12,13 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface VPDeviceHadjCountModel : NSObject
 
+// 通道 1byte
+@property (nonatomic, assign) short channel;
+// 所有通道总计数 4byte
+@property (nonatomic, assign) long totalCount;
 // 当前包诵经计数 2byte
+//  单通道下，如果 currentCount == totalCount 则表示读取完成
+//  多通道下，所有model的currentCount累加 == totalCount 则表示读取完成
 @property (nonatomic, assign) int currentCount;
 // 开始时间 时间戳 4byte
 @property (nonatomic, assign) long startTimestamp;
